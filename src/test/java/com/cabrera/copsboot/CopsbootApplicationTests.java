f2bcf0f649a2d670c09446f22ce0f18df0b4f27e
Cabrera <cabrademontes@gmail.com>
2025-04-27 14:51:49 -0600

    first commit

---
+++ src/test/java/com/cabrera/copsboot/CopsbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabrera.copsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopsbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
