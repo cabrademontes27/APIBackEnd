c6768d4c04d5ba435eec2f55b891f4f8ffa796cf
Cabrera <cabrademontes@gmail.com>
2025-04-30 05:14:07 -0600

    Add AuthServerId entity

--- src/main/java/com/cabrera/copsboot/user/domain/AuthServerId.java
+++ src/main/java/com/cabrera/copsboot/user/domain/AuthServerId.java
@@ -1,4 +1,35 @@
 package com.cabrera.copsboot.user.domain;
 
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Embeddable
 public class AuthServerId {
+    private UUID value;
+
+    protected AuthServerId() {
+    }
+
+    public AuthServerId(UUID value) {
+        this.value = value;
+    }
+
+    public UUID value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AuthServerId)) return false;
+        AuthServerId that = (AuthServerId) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
