85df2623ab65129554bc1c381a5ff627d3c25f88
Cabrera <cabrademontes@gmail.com>
2025-04-30 05:14:07 -0600

    Implement attribute converter for AuthServerId

--- src/main/java/com/cabrera/copsboot/user/domain/AuthServerIdAttributeConverter.java
+++ src/main/java/com/cabrera/copsboot/user/domain/AuthServerIdAttributeConverter.java
@@ -1,4 +1,20 @@
 package com.cabrera.copsboot.user.domain;
 
-public class AuthServerIdAttributeConverter {
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.UUID;
+
+@Converter(autoApply = true)
+public class AuthServerIdAttributeConverter implements AttributeConverter<AuthServerId, UUID> {
+
+    @Override
+    public UUID convertToDatabaseColumn(AuthServerId attribute) {
+        return attribute.value();
+    }
+
+    @Override
+    public AuthServerId convertToEntityAttribute(UUID dbData) {
+        return new AuthServerId(dbData);
+    }
 }
