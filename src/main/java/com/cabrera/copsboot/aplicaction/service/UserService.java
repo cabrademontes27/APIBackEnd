efe3df0db096dc20fe92a5c38b392756e01b13c6
Cabrera <cabrademontes@gmail.com>
2025-04-30 05:13:44 -0600

    Adjust user creation logic

--- src/main/java/com/cabrera/copsboot/aplicaction/service/UserService.java
+++ src/main/java/com/cabrera/copsboot/aplicaction/service/UserService.java
@@ -1,4 +1,28 @@
 package com.cabrera.copsboot.aplicaction.service;
 
+
+import com.cabrera.copsboot.user.domain.*;
+import com.cabrera.copsboot.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findUserByAuthServerId(AuthServerId authServerId) {
+        return userRepository.findByAuthServerId(authServerId);
+    }
+
+    public User createUser(CreateUserParameters parameters) {
+        UserId userId = userRepository.nextId();
+        User user = new User(userId, parameters.email(), parameters.authServerId(), parameters.mobileToken());
+        return userRepository.save(user);
+    }
 }
