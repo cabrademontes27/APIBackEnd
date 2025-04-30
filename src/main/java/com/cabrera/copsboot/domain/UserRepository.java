e417b8f1e9cefd4d75876cf645e15fe67e54ea86
Cabrera <cabrademontes@gmail.com>
2025-04-29 18:53:10 -0600

    move repository to separate repository package

--- src/main/java/com/cabrera/copsboot/domain/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cabrera.copsboot.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.UUID;
-
-public interface UserRepository extends CrudRepository<User, UUID> {
-    
-}
