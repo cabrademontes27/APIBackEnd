8ae284e091c046fd50ea785612f5802e371eee08
Cabrera <cabrademontes@gmail.com>
2025-04-30 05:14:07 -0600

    Extend UserRepository with custom finders

--- src/main/java/com/cabrera/copsboot/user/repository/UserRepository.java
+++ src/main/java/com/cabrera/copsboot/user/repository/UserRepository.java
@@ -1,11 +1,16 @@
-package com.cabrera.copsboot.repository;
+package com.cabrera.copsboot.user.repository;
 
-import com.cabrera.copsboot.domain.User;
-import com.cabrera.copsboot.domain.UserId;
+import com.cabrera.copsboot.user.domain.AuthServerId;
+import com.cabrera.copsboot.user.domain.User;
+import com.cabrera.copsboot.user.domain.UserId;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, UserId> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByAuthServerId(AuthServerId authServerId);
+    default UserId nextId() {
+        return new UserId(java.util.UUID.randomUUID());
+    }
 }
