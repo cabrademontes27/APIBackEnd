af5f4d15927f071bf736ef3722e3cfcbce09672d
Cabrera <cabrademontes@gmail.com>
2025-04-29 18:54:40 -0600

    feat(domain): add base abstract classes for strongly typed entity IDs

---
+++ src/main/java/com/cabrera/copsboot/domain/AbstractEntity.java
@@ -0,0 +1,38 @@
+package com.cabrera.copsboot.domain;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.MappedSuperclass;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity<T extends AbstractEntityId<?>> {
+
+    @EmbeddedId
+    private T id;
+
+    protected AbstractEntity() {
+    }
+
+    protected AbstractEntity(T id) {
+        this.id = Objects.requireNonNull(id);
+    }
+
+    public T getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof AbstractEntity))
+            return false;
+        AbstractEntity<?> that = (AbstractEntity<?>) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/cabrera/copsboot/domain/AbstractEntityId.java
@@ -0,0 +1,44 @@
+package com.cabrera.copsboot.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public abstract class AbstractEntityId<T extends Serializable> implements Serializable {
+    private final T id;
+
+    protected AbstractEntityId() {
+        this.id = null;
+    }
+
+    protected AbstractEntityId(T id) {
+        this.id = Objects.requireNonNull(id);
+    }
+
+    public T getId() {
+        return id;
+    }
+
+    public String asString() {
+        return id.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof AbstractEntityId))
+            return false;
+        AbstractEntityId<?> that = (AbstractEntityId<?>) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" + id + ")";
+    }
+}
