6924a90c08b957628eeca8f321a402a6e0509a6e
Cabrera <cabrademontes@gmail.com>
2025-04-30 05:14:07 -0600

    Update request logging setup

--- src/main/java/com/cabrera/copsboot/config/RequestLoggerFilter.java
+++ src/main/java/com/cabrera/copsboot/config/RequestLoggerFilter.java
@@ -1,4 +1,18 @@
 package com.cabrera.copsboot.config;
 
-public class RequestLoggerFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RequestLoggerFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        System.out.println("📡 [Filtro] Petición recibida: " + req.getMethod() + " " + req.getRequestURI());
+        chain.doFilter(request, response);
+    }
 }
